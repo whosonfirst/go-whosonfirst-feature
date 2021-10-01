a0291972978b6e8a6ed19ebce0cc344d56347625
thisisaaronland <aaron@localhost>
2021-10-01 14:51:24 -0700

    add geometry.Centroid method

---
+++ geometry/centroid_test.go
@@ -0,0 +1,127 @@
+package geometry
+
+import (
+	"io"
+	"strings"
+	"testing"
+)
+
+var lbl_centroid = strings.NewReader(`{"properties": {"lbl:latitude":41.933211, "lbl:longitude":-87.677286, "reversegeo:latitude":41.943403, "reversegeo:longitude":-87.665224}}`)
+
+var reversegeo_centroid = strings.NewReader(`{"properties": {"reversegeo:latitude":41.943403, "reversegeo:longitude":-87.665224, "geom:latitude":41.943648, "geom:longitude":-87.665224}}`)
+
+var geom_centroid = strings.NewReader(`{"properties": {"geom:latitude":41.943648, "geom:longitude":-87.665224}}`)
+
+var null_centroid = strings.NewReader(`{"properties": {}}`)
+
+func TestLblCentroid(t *testing.T) {
+
+	body, err := io.ReadAll(lbl_centroid)
+
+	if err != nil {
+		t.Fatalf("Failed to read data , %v", err)
+	}
+
+	pt, source, err := Centroid(body)
+
+	if err != nil {
+		t.Fatalf("Failed to derive centroid (lbl), %v", err)
+	}
+
+	if source != "lbl" {
+		t.Fatalf("Unexpected source")
+	}
+
+	if pt.Lat() != 41.933211 {
+		t.Fatalf("Unexpected latitude")
+	}
+
+	if pt.Lon() != -87.677286 {
+		t.Fatalf("Unexpected longitude")
+	}
+
+}
+
+func TestReversegeoCentroid(t *testing.T) {
+
+	body, err := io.ReadAll(reversegeo_centroid)
+
+	if err != nil {
+		t.Fatalf("Failed to read data , %v", err)
+	}
+
+	pt, source, err := Centroid(body)
+
+	if err != nil {
+		t.Fatalf("Failed to derive centroid, %v", err)
+	}
+
+	if source != "reversegeo" {
+		t.Fatalf("Unexpected source")
+	}
+
+	if pt.Lat() != 41.943403 {
+		t.Fatalf("Unexpected latitude")
+	}
+
+	if pt.Lon() != -87.665224 {
+		t.Fatalf("Unexpected longitude")
+	}
+
+}
+
+func TestGeomCentroid(t *testing.T) {
+
+	body, err := io.ReadAll(geom_centroid)
+
+	if err != nil {
+		t.Fatalf("Failed to read data , %v", err)
+	}
+
+	pt, source, err := Centroid(body)
+
+	if err != nil {
+		t.Fatalf("Failed to derive centroid, %v", err)
+	}
+
+	if source != "geom" {
+		t.Fatalf("Unexpected source")
+	}
+
+	if pt.Lat() != 41.943648 {
+		t.Fatalf("Unexpected latitude")
+	}
+
+	if pt.Lon() != -87.665224 {
+		t.Fatalf("Unexpected longitude")
+	}
+
+}
+
+func TestNullCentroid(t *testing.T) {
+
+	body, err := io.ReadAll(null_centroid)
+
+	if err != nil {
+		t.Fatalf("Failed to read data , %v", err)
+	}
+
+	pt, source, err := Centroid(body)
+
+	if err != nil {
+		t.Fatalf("Failed to derive centroid (null), %v", err)
+	}
+
+	if source != "nullisland" {
+		t.Fatalf("Unexpected source")
+	}
+
+	if pt.Lat() != 0.0 {
+		t.Fatalf("Unexpected latitude")
+	}
+
+	if pt.Lon() != 0.0 {
+		t.Fatalf("Unexpected longitude")
+	}
+
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/whosonfirst/go-whosonfirst-feature
 go 1.16
 
 require (
+	github.com/paulmach/orb v0.2.2
 	github.com/sfomuseum/go-edtf v0.3.0
 	github.com/tidwall/gjson v1.9.1
 	github.com/whosonfirst/go-whosonfirst-flags v0.4.3
